0bc47bb8d758f2d2b6cf9e75ee6250d5e4ed4153
vjy-07 <gvijaycrl@gmail.com>
2025-11-21 19:22:46 +0530

    solve two sum

---
+++ Arrays and Hashing/1_Two_Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++){
+            int rem = target - nums[i];
+            if(mp.find(rem )!=mp.end()){
+                return {i,mp[rem]};
+            }
+            mp[nums[i]]=i;
+        }
+        return {-1,-1};
+    }
+};
+
+TC - O(n)
+SC - O(n)
