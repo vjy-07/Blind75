7c01bc2b90ff2f4af6e457f29143a41423a9a47b
vjy-07 <gvijaycrl@gmail.com>
2025-11-22 10:49:51 +0530

    solve 3sum

---
+++ 2. Two Pointers/15_3Sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            int j=i+1;
+            int k=n-1;
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum>0) k--;
+                else if(sum<0) j++;
+                else{
+                    vector<int> temp = {nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
+                    j++;k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+TC - O(n^2)
+SC - O(1)
