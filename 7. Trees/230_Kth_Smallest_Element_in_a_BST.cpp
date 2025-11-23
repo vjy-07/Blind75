2155be186fded63af96bc64ccfa440620d791a09
vjy-07 <gvijaycrl@gmail.com>
2025-11-23 22:31:42 +0530

    solve Kth Smallest Element in a BST

---
+++ 7. Trees/230_Kth_Smallest_Element_in_a_BST.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void solve(TreeNode* root, int &cnt, int &ans, int k){
+        if(root==NULL) return;
+        solve(root->left,cnt,ans,k);
+        cnt++;
+        if(cnt==k){
+            ans=root->val;
+            return;
+        }
+        solve(root->right,cnt,ans,k);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        int cnt =0;
+        int ans;
+        solve(root,cnt,ans,k);
+        return ans;
+    }
+};
+
+TC - O(n)
+SC - O(n)
