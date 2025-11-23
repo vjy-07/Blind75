af201df39fb91ecd23801e28bce0407aa4ae86ab
vjy-07 <gvijaycrl@gmail.com>
2025-11-23 20:57:13 +0530

    solve Binary Tree Level Order Traversal

---
+++ 7. Trees/102_Binary_Tree_Level_Order_Traversal.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        if(root==NULL) return ans;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> level;
+            for(int i=0;i<size;i++){
+                TreeNode* node = q.front();q.pop();
+                if(node->left!=nullptr) q.push(node->left);
+                if(node->right!=nullptr) q.push(node->right);
+                level.push_back(node->val);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
+
+TC - O(n)
+SC - O(n)
