fe2d14fe088079bb85849775d31c809e26c7f35b
vjy-07 <gvijaycrl@gmail.com>
2025-11-24 22:06:10 +0530

    solve Missing Number

---
+++ 8. Bit Manipulation/268_Missing_Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n= nums.size();
+        int xor1 = 0, xor2 = 0;
+        for(int i=0;i<n;i++){
+            xor2 = xor2^nums[i];
+            xor1 = xor1 ^ (i+1);
+        }
+        return xor1 ^ xor2;
+    }
+};
+
+TC - O(n)
+SC - O(1)
