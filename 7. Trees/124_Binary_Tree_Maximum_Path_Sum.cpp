17a89e9fdf73c570dd0bea7dc906f1368731be9a
vjy-07 <gvijaycrl@gmail.com>
2025-12-01 21:39:30 +0530

    solve Binary Tree Maximum Path Sum

---
+++ 7. Trees/124_Binary_Tree_Maximum_Path_Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int helper(TreeNode* root, int& maxi){
+        if(root==NULL) return 0;
+        int lh = max(0, helper(root->left, maxi));
+        int rh = max(0, helper(root->right, maxi));
+        maxi = max(maxi, root->val+lh+rh);
+        return root->val + max(lh,rh);
+    }
+    int maxPathSum(TreeNode* root) {
+        int maxi = INT_MIN;
+        helper(root,maxi);
+        return maxi;
+    }
+};
+
+TC - O(n)
+SC - O(h)
